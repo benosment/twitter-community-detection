f62435a77ce934247cec439adc24b0778b2c8102
Benjamin Osment <benjamin.j.osment@gmail.com>
2013-04-30 21:09:21 -0400

    changed graph.py uses DB tweets instead of canned tweets

--- db.py
+++ db.py
@@ -1,24 +1,38 @@
+from pymongo import MongoClient
+
 def connect_db():
   client = MongoClient()
   db = client.twitter_database
   return db
 
+def connect_tweets_db():
+  db = connect_db()
+  return db.tweets
+
 def insert_tweet(tweet):
   ''' 
       Tweet is expected to be in json format
   '''
-  db = connect_db()
-  tweets = db.tweets
+  tweets = connect_tweets_db()
   tweets.insert(tweet)  
 
 def filter_tweets():
   ''' 
       Remove all tweets that do not have a mention or RT
   '''
-  db = connect_db()
-  tweets = db.tweets
+  tweets = connect_tweets_db()
   tweets.remove({'entities.user_mentions' : []}) # find tweets without a mention
 
 def find_retweets():
   # find retweets tweets, also shows up as a user_mention
-  tweets.find({'text' : {'$regex' : '^RT'}}) 
+  tweets = connect_tweets_db()
+  return tweets.find({'text' : {'$regex' : '^RT'}}) 
+
+def get_tweets():
+  # return all tweets, returns a cursor
+  tweets = connect_tweets_db()
+  return tweets.find()
+
+def get_num_tweets():
+  cursor = get_tweets()
+  return cursor.count()
--- graph.py
+++ graph.py
@@ -1,35 +1,52 @@
 from sample import sample_tweets
+from bson.objectid import ObjectId
+
 import networkx as nx
 import matplotlib.pyplot as plt
 import pdb
+import sys
+import db
 
 def get_mentions(tweet):
   return tweet['entities']['user_mentions']
 
-def create_graph():
-  g = nx.DiGraph() # do we care about the direction? 
-  global sample_tweets
-  for tweet in sample_tweets:
-    mentions = get_mentions(tweet)
-    #pdb.set_trace()
-    for mention in mentions:
-      g.add_edge(tweet['user']['screen_name'], 
+def create_graph(tweets_cursor):
+  g = nx.DiGraph() # do we care about the direction? multigraph?
+  count = 0
+  try: 
+    while True:
+      if count % 100 == 0:
+        print "processing tweet %d" % count
+      count += 1
+      tweet = tweets_cursor.next()
+      mentions = get_mentions(tweet)
+      #pdb.set_trace()
+      for mention in mentions:
+        g.add_edge(tweet['user']['screen_name'], 
                  mention['screen_name'],
                  text = tweet['text'],
                  weight = 1)
+  except StopIteration:
+    pass # ok
+  except Exception,e:
+    # log instead? 
+    print "Exception", e
+    sys.exit(-1)
   return g
 
 def print_summary(graph):
     print "Number of nodes: %d" % graph.number_of_nodes()
     print "Number of edges: %d" % graph.number_of_edges()
-    print "Node degrees:", sorted(nx.degree(graph))
 
-def save_graph(graph, filename='graph.pdf'):
+def print_graph(graph, filename='graph.pdf'):
     pos = nx.spring_layout(graph)
     nx.draw(graph, pos)
     plt.savefig(filename, dpi=1000)
   
 if __name__ == '__main__':
-    g = create_graph()
+    tweets = db.get_tweets()
+    g = create_graph(tweets)
     print_summary(g)
-    save_graph(g)
+    print_graph(g)
+    # how to save? 
+    #save_graph(g)
