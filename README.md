9f2cd6fa30846cad8f7567d7470115c6cec596dc
Benjamin Osment <benjamin.j.osment@gmail.com>
2013-05-04 16:24:24 -0400

    added silhouette score calculation

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
-twitter-ID-CD
-=============
+Twitter Community Detection
+===========================
 
-Information Diffusion and Community Detection within the Twitter
-Domain
+Finds Communities within the Twitter domain using Spectral Clustering
+and Influence. 
+
+Steps:
+ 1. Harvest Tweets using streaming API (stream.py), store tweets in MongoDB
+ 2. Remove any tweets that do not have a retweet/mention
+ 3. Construct a graph from the remaining tweets
+ 4. Use Spectral Clustering to find the best clusters
+ 5. Evaluation Clustering using the Silhouette score 
 
 Requires the following modules:
  - tweepy
+ - pymongo
  - networkx
  - matplotlib
- - pymongo
+ - numpy
+ - sklearn
+
+
+Peter J. Rousseeuw (1987). “Silhouettes: a Graphical Aid to the
+Interpretation and Validation of Cluster Analysis”. Computational and
+Applied Mathematics 20: 53–65. doi:10.1016/0377-0427(87)90125-7.
--- cluster.py
+++ cluster.py
@@ -5,40 +5,12 @@ import numpy as np
 from scipy import linalg
 from scipy.cluster.vq import kmeans2
 from sklearn.cluster import spectral_clustering
-def scluster(subgraph):
-  pass
+from sklearn import metrics
 
-def diag_example():
-  a = np.matrix([[4, 1], [-8, -5]])
-  d = a.diagonal()
-  print "Adjacency Matrix:", a
-  print "Diagonal Matrix:", d
-
-def eig_example():
-  A = np.array([[1,2], [3,4]])
-  la,v = linalg.eig(A)
-  l1,l2 = la
-  print "Eigenvalues: ", l1, l2 
-  print "First eigenvector: ", v[:,0]
-  print "Second eigenvector: ", v[:,1]
-  
-def scluster_example():
-  A = np.array([[0,1,1,1,0,0,0,0,0],
-                [1,0,1,0,0,0,0,0,0],
-                [1,1,0,1,0,0,0,0,0],
-                [1,0,1,0,1,1,0,0,0],
-                [0,0,0,1,0,1,1,1,0],
-                [0,0,0,1,1,0,1,1,0],
-                [0,0,0,0,1,1,0,1,1],
-                [0,0,0,0,1,1,1,0,0],
-                [0,0,0,0,0,0,1,0,0]])
-  print scluster(A, 3)
-
-
-def scluster(G, num_clusters=3):
+def scluster(G, num_clusters=3, threshold=2):
   # given a graph, calculate an affinity matrix
-  #D = distance(G)
-  A = affinity(G)
+  D = distance(G)
+  A = affinity(D, threshold=threshold)
   # given an affinity matrix
   # calculate the diagonal
   D = diagonal(A)
@@ -53,11 +25,19 @@ def scluster(G, num_clusters=3):
   S = np.array([vals[k] for k in keys[:num_clusters]]).transpose()
   # return the labels based on kmeans of smallest eigenvectors
   clusters, labels = kmeans2(S, k=num_clusters, iter=30, minit='points')
-  return labels
+  # calculate silhouette score
+  s_score = metrics.silhouette_score(S, labels)
+  return labels, s_score
 
 def scluster2(G, num_clusters=3):
+  n = G.number_of_nodes()
+  if n < 100:
+    n = n / 10 + 1
+  else:
+    n = n / 100 + 1
+  print n
   D = distance(G)
-  A = affinity(D)
+  A = affinity(D, threshold = n)
   labels = spectral_clustering(A, n_clusters=num_clusters)
   return labels
 
@@ -67,28 +47,20 @@ def distance(G):
   n = G.number_of_nodes()
   D = np.zeros((n,n))
   nodes = G.nodes()
-  for i in range(len(nodes)):
-    for j in range(len(nodes)):
+  for i in range(n):
+    for j in range(n):
       D[i][j] = len(nx.shortest_path(G, nodes[i], nodes[j])) - 1
   return D
 
-def affinity(G, closeness=2):
+def affinity(D, threshold=2):
   "Given a graph, calculate affinity"
-  n = G.number_of_nodes()
+  n = D.shape[0]
   A = np.zeros((n,n))
   # create node string -> index mapping and
   # index -> node string mapping
-  i = 0
-  i2s = {}
-  s2i = {}
-  for node in G.nodes():
-    i2s[i] = node
-    s2i[node] = i
-    i +=1 
-  # affinty based on direct neighbors
-  for (v,w) in G.edges():
-    A[s2i[v]][s2i[w]] = 1
-    A[s2i[w]][s2i[v]] = 1
+  for i in range(n):
+    for j in range(n):
+      A[i][j] = D[i][j] if D[i][j] <= threshold else 0
   return A
 
 def diagonal(A):
@@ -118,16 +90,29 @@ def sample_graph():
   g.add_edge(7,9)
   return g
 
+def find_best_params(G):
+  highest_score = 0
+  highest_num_cluster = 0
+  highest_threshold = 0
+  for i in range(2,10):
+    for j in range(2,10):
+      labels, s_score = scluster(g, i, j)
+      if s_score > highest_score:
+        highest_score = s_score
+        highest_num_cluster = i
+        highest_threshold = j
+  return (highest_score, highest_num_cluster, highest_threshold)
+
 if __name__ == '__main__':
-  g = graph.get_graph('/data/512/100-twitter.gexf', limit=100)
+  g = graph.get_graph('/data/512/10000-twitter.gexf', limit=10000)
   top_subgraphs = graph.get_subgraphs(g, k=1)
   g = top_subgraphs[0]
-  graph.display_graph(g)
-  labels = scluster2(g,2)
-  graph.display_graph_clusters(g,labels)
+  #graph.display_graph(g)
+  #graph.display_graph_clusters(g,labels)
+  print find_best_params(g)
 
   # working!!
   # g = sample_graph()
   # graph.display_graph(g)
-  # labels = scluster(g,3)
+  # labels = scluster2(g,2)
   # graph.display_graph_clusters(g,labels)
