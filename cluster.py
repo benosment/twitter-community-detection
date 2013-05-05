a1e910878c75b18cf874b03a241d48cfbbbb9058
Benjamin Osment <benjamin.j.osment@gmail.com>
2013-05-05 18:09:16 -0400

    misc changes to cluster.py

--- cluster.py
+++ cluster.py
@@ -29,17 +29,28 @@ def scluster(G, num_clusters=3, threshold=2):
   s_score = metrics.silhouette_score(S, labels)
   return labels, s_score
 
-def scluster2(G, num_clusters=3):
-  n = G.number_of_nodes()
-  if n < 100:
-    n = n / 10 + 1
-  else:
-    n = n / 100 + 1
-  print n
-  D = distance(G)
-  A = affinity(D, threshold = n)
-  labels = spectral_clustering(A, n_clusters=num_clusters)
-  return labels
+def weighted_scluster(G, num_clusters=3, threshold=2, important = []):
+  # given a graph, calculate an affinity matrix
+  # for weighted scluster, important people are easier to get to/from
+  D = weighted_distance(G, important)
+  A = affinity(D, threshold=threshold)
+  # given an affinity matrix
+  # calculate the diagonal
+  D = diagonal(A)
+  # calculate the Laplacian matrix, L
+  L = D - A
+  # find the eigenvalues and vectors of L
+  eigen_values, eigen_vectors = linalg.eig(L)
+  # use the smallest eigenvectors
+  vals = dict(zip(eigen_values, eigen_vectors.transpose()))
+  keys = vals.keys()
+  keys.sort()
+  S = np.array([vals[k] for k in keys[:num_clusters]]).transpose()
+  # return the labels based on kmeans of smallest eigenvectors
+  clusters, labels = kmeans2(S, k=num_clusters, iter=30, minit='points')
+  # calculate silhouette score
+  s_score = metrics.silhouette_score(S, labels)
+  return labels, s_score
 
 
 def distance(G):
@@ -50,6 +61,19 @@ def distance(G):
   for i in range(n):
     for j in range(n):
       D[i][j] = len(nx.shortest_path(G, nodes[i], nodes[j])) - 1
+  return D
+
+def weighted_distance(G, important):
+  "Given a graph, return a distance matrix"
+  n = G.number_of_nodes()
+  D = np.zeros((n,n))
+  nodes = G.nodes()
+  for i in range(n):
+    for j in range(n):
+      distance = len(nx.shortest_path(G, nodes[i], nodes[j])) - 1
+      if nodes[i] or nodes[j] in important:
+        distance *= 0.9  # easier to get to important nodes
+      D[i][j] = distance
   return D
 
 def affinity(D, threshold=2):
@@ -101,18 +125,55 @@ def find_best_params(G):
         highest_score = s_score
         highest_num_cluster = i
         highest_threshold = j
+      print i, j, s_score
   return (highest_score, highest_num_cluster, highest_threshold)
 
+def find_best_weighted_params(G):
+  highest_score = 0
+  highest_num_cluster = 0
+  highest_threshold = 0
+  for i in range(2,10):
+    important = find_important(g, i)
+    for j in range(2,10):
+      labels, s_score = weighted_scluster(g, i, j, important)
+      if s_score > highest_score:
+        highest_score = s_score
+        highest_num_cluster = i
+        highest_threshold = j
+      print i, j, s_score
+  return (highest_score, highest_num_cluster, highest_threshold)
+
+def find_important(G, n):
+  scores = nx.betweenness_centrality(G).items()
+  l = [ ] 
+  for (item, score) in scores:
+    l.append((score, item))
+  l.sort(reverse=True)
+  important = []
+  for i in range(n):
+    important.append(l[i][1])
+  return important
+  
 if __name__ == '__main__':
   g = graph.get_graph('/data/512/10000-twitter.gexf', limit=10000)
   top_subgraphs = graph.get_subgraphs(g, k=1)
   g = top_subgraphs[0]
-  #graph.display_graph(g)
-  #graph.display_graph_clusters(g,labels)
-  print find_best_params(g)
+  graph.display_graph(g)
+  #t = find_best_params(g)
+  #t2 = find_best_weighted_params(g)
 
-  # working!!
+  labels, score_1 = scluster(g, 4, 3)
+  graph.display_graph_clusters(g, labels)
+
+  important = find_important(g, 4)
+  labels, score_2 = weighted_scluster(g, 4, 3, important)
+  graph.display_graph_clusters(g, labels)
+
+  print score_1, score_2
+
+  # Sample
   # g = sample_graph()
+  # important = find_important(g, 2)
   # graph.display_graph(g)
-  # labels = scluster2(g,2)
-  # graph.display_graph_clusters(g,labels)
+  # labels, s_score = weighted_scluster(g, 2, 1, important)
+  # graph.display_graph_clusters(g, labels)
